01b1c0a3ea2efe080b7a911fe523eb75021711d2
Neruni <nerunisal@gmail.com>
2022-11-13 22:50:26 -0700

    formated files. updated read me.

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ An in-depth paragraph about your project and overview of use.
 ### Dependencies
 
 * Describe any prerequisites, libraries, OS version, etc., needed before installing program.
-* ex. Windows 10
+
 
 ### Installing
 
@@ -36,6 +36,3 @@ command to run if program contains helper info
 ## Authors
 
 Contributors names and contact info
-
-ex. Dominique Pizzie  
-ex. [@DomPizzie](https://twitter.com/dompizzie)
--- components/headers/LandingPageHeader.jsx
+++ components/headers/LandingPageHeader.jsx
@@ -1,17 +1,17 @@
-function LandingPageHeader(props) {  
+function LandingPageHeader(props) {
     return (
         <header className=' h-screen p-8 bg-gradient-to-r from-rose-100 to-teal-100'>
-            <img className='w-[70px] mx-auto pb-10' src="logo.svg" alt="next best developer"/>
-             <h1 className='font-bold pb-10 text-4xl text-indigo-500 text-center
+            <img className='w-[70px] mx-auto pb-10' src="logo.svg" alt="next best developer" />
+            <h1 className='font-bold pb-10 text-4xl text-indigo-500 text-center
                 md:text-3xl lg:text-5xl xl:text-6xl
-            '> 
+            '>
                 Next Best Dev
             </h1>
 
             <h3 className='font-bold text-4xl text-sky-900 text-center
                 md:text-2xl lg:text-2xl xl:text-2xl
             '>
-                 Finding the perfect next best dev for your buisness
+                Finding the perfect next best dev for your buisness
             </h3>
 
         </header>
