5bac51cb4fe681cd56986c1b4156309a74ca406a
Neruni <nerunisal@gmail.com>
2022-11-13 22:14:30 -0700

    updated index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,15 @@
-import Head from 'next/head' 
-import {LandingPageHeader} from './../components/headers'
+import Head from 'next/head'
+import { LandingPageHeader } from './../components/headers'
 
 export default function Home() {
   return (
-        <>
-          <Head>
-            <meta name="description" content="find the next best react developer for your buisness. Next Best Dev gives you access to North Americas largest pool of React Developers." />
-            <title>Assessment 1</title>
-          </Head> 
-          <LandingPageHeader title="My Title" tagline="My Tagline" />
-        </>
+    <>
+      <Head>
+        <meta name="description" content="find the next best react developer for your buisness. Next Best Dev gives you access to North Americas largest pool of React Developers." />
+        <title>Assessment 1</title>
+      </Head>
+      <LandingPageHeader title="My Title" tagline="My Tagline" />
+    </>
   );
 }
 
